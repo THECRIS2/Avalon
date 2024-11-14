120ddbf48747a899351a02b3fd2b6a6690493da9
THECRIS2 <moralesrodriguezcristiandavid8@gmail.com>
2024-11-14 20:39:39 -0300

    Version 0.4.3

--- app/src/main/java/com/ni/avalon/activities/menu.java
+++ app/src/main/java/com/ni/avalon/activities/menu.java
@@ -3,10 +3,13 @@ package com.ni.avalon.activities;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -14,13 +17,25 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
 import com.ni.avalon.R;
 import com.ni.avalon.databinding.ActivityMenuBinding;
+import com.ni.avalon.model.UserModel;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class menu extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMenuBinding binding;
+    FirebaseStorage storage;
+    FirebaseAuth auth;
+    FirebaseDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +43,8 @@ public class menu extends AppCompatActivity {
 
         binding = ActivityMenuBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        db = FirebaseDatabase.getInstance();
 
         setSupportActionBar(binding.appBarMenu.toolbar);
         DrawerLayout drawer = binding.drawerLayout;
@@ -41,6 +58,28 @@ public class menu extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_menu);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        View headerView = navigationView.getHeaderView(0);
+        TextView headernombre = headerView.findViewById(R.id.nombrePerfil);
+        TextView headeremail = headerView.findViewById(R.id.correoPerfil);
+        CircleImageView headerImg = headerView.findViewById(R.id.nav_header_img);
+
+        db.getReference().child("Usuarios").child(FirebaseAuth.getInstance().getUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        UserModel userModel = snapshot.getValue(UserModel.class);
+
+                        headernombre.setText(userModel.getNombre());
+                        headeremail.setText(userModel.getCorreo());
+                        Glide.with(menu.this).load(userModel.getProfileImg()).into(headerImg);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
     }
 
     @Override
