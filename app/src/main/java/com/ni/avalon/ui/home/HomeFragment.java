395dbbaf34ae34bfde9ce15af31570562b98c550
THECRIS2 <moralesrodriguezcristiandavid8@gmail.com>
2024-11-15 17:08:39 -0300

    Version 0.4.4 (sera la ultima version con el nombre en clave avalon)

--- app/src/main/java/com/ni/avalon/ui/home/HomeFragment.java
+++ app/src/main/java/com/ni/avalon/ui/home/HomeFragment.java
@@ -1,9 +1,12 @@
 package com.ni.avalon.ui.home;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -17,6 +20,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -24,10 +28,12 @@ import com.ni.avalon.R;
 import com.ni.avalon.adapters.HomeAdapter;
 import com.ni.avalon.adapters.PopularAdapters;
 import com.ni.avalon.adapters.RecommendedAdapter;
+import com.ni.avalon.adapters.ViewAllAdapter;
 import com.ni.avalon.databinding.FragmentHomeBinding;
 import com.ni.avalon.model.HomeCategory;
 import com.ni.avalon.model.PopularModel;
 import com.ni.avalon.model.RecommendedModel;
+import com.ni.avalon.model.ViewAllModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -51,6 +57,11 @@ public class HomeFragment extends Fragment {
     // recomendados
     RecommendedAdapter recommendedAdapter;
     List<RecommendedModel> recommendedModelList;
+
+    EditText barraBusqueda;
+    private List<ViewAllModel> viewAllModelList;
+    private RecyclerView recyclerViewBuscar;
+    private ViewAllAdapter viewAllAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -146,7 +157,60 @@ public class HomeFragment extends Fragment {
                     }
                 });
 
+
+        // barra de busqueda
+        barraBusqueda = root.findViewById(R.id.barraBusqueda);
+        recyclerViewBuscar = root.findViewById(R.id.res_busqueda);
+        recyclerViewBuscar.setLayoutManager(new LinearLayoutManager(getContext()));
+        viewAllModelList = new ArrayList<>();
+        viewAllAdapter = new ViewAllAdapter(getContext(),viewAllModelList);
+        recyclerViewBuscar.setAdapter(viewAllAdapter);
+        recyclerViewBuscar.setHasFixedSize(true);
+        barraBusqueda.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                if (s.toString().isEmpty()){
+                    viewAllModelList.clear();
+                    viewAllAdapter.notifyDataSetChanged();
+                } else {
+                    searchProduct(s.toString());
+                }
+            }
+        });
+
         return root;
+    }
+
+    private void searchProduct(String nombre) {
+        if (!nombre.isEmpty()) { // Cambia la condición para verificar que tipo no está vacío
+            db.collection("AllProducts").whereEqualTo("nombre", nombre).get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful() && task.getResult() != null) {
+                                viewAllModelList.clear(); // Limpia la lista para mostrar solo resultados nuevos
+                                for (DocumentSnapshot doc : task.getResult().getDocuments()) {
+                                    ViewAllModel viewAllModel = doc.toObject(ViewAllModel.class);
+                                    viewAllModelList.add(viewAllModel);
+                                }
+                                viewAllAdapter.notifyDataSetChanged(); // Notifica cambios al adaptador
+                            } else {
+                                Toast.makeText(getContext(), "No se encontraron productos", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
     }
 
 }
--- app/src/main/java/com/ni/avalon/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/ni/avalon/ui/profile/ProfileFragment.java
@@ -1,6 +1,8 @@
 package com.ni.avalon.ui.profile;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -29,6 +31,7 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.ni.avalon.R;
+import com.ni.avalon.activities.MainActivity;
 import com.ni.avalon.model.UserModel;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -37,7 +40,7 @@ public class ProfileFragment extends Fragment {
 
     CircleImageView perfilImg;
     EditText nombre, apellido, fnacimiento, correo, contraseña;
-    Button actualizar;
+    Button actualizar, logoff;
     FirebaseStorage storage;
     FirebaseAuth auth;
     FirebaseDatabase db;
@@ -58,19 +61,25 @@ public class ProfileFragment extends Fragment {
         correo = root.findViewById(R.id.profile_email);
         contraseña = root.findViewById(R.id.profile_contraseña);
         actualizar = root.findViewById(R.id.update_profile);
+        logoff = root.findViewById(R.id.logoff_profile);
 
         db.getReference().child("Usuarios").child(FirebaseAuth.getInstance().getUid())
                         .addListenerForSingleValueEvent(new ValueEventListener() {
                             @Override
                             public void onDataChange(@NonNull DataSnapshot snapshot) {
                                 UserModel userModel = snapshot.getValue(UserModel.class);
+                                nombre.setText(userModel.getNombre());
+                                apellido.setText(userModel.getApellido());
+                                fnacimiento.setText(userModel.getFnacimiento());
+                                correo.setText(userModel.getCorreo());
+                                contraseña.setText(userModel.getContraseña());
 
                                 Glide.with(getContext()).load(userModel.getProfileImg()).into(perfilImg);
                             }
 
                             @Override
                             public void onCancelled(@NonNull DatabaseError error) {
-
+                                Toast.makeText(getContext(), "Error al cargar los datos", Toast.LENGTH_SHORT).show();
                             }
                         });
 
@@ -91,10 +100,52 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+        //Cerrar sesion
+        logoff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Eliminar preferencia de recordar sesión
+                SharedPreferences.Editor editor = getContext().getSharedPreferences("login_prefs", Context.MODE_PRIVATE).edit();
+                editor.putBoolean("remember_me", false);
+                editor.apply();
+
+                // Cerrar sesión de Firebase
+                auth.signOut();
+
+                // Reedirigira al usuario al activity de nuestro login
+                Intent intent = new Intent(getContext(), MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Limpiar el stack de actividades
+                startActivity(intent);
+            }
+        });
+
         return root;
     }
 
     private void actualizarPerfilUsuario() {
+        String nuevoNombre = nombre.getText().toString();
+        String nuevoApellido = apellido.getText().toString();
+        String nuevaFechaNacimiento = fnacimiento.getText().toString();
+        String nuevoCorreo = correo.getText().toString();
+        String nuevaContraseña = contraseña.getText().toString();
+
+        // Actualizar los datos en Firebase
+        UserModel usuarioActualizado = new UserModel(nuevoNombre, nuevoApellido, nuevaFechaNacimiento, nuevoCorreo, nuevaContraseña);
+        db.getReference().child("Usuarios").child(auth.getUid())
+                .setValue(usuarioActualizado)
+                .addOnSuccessListener(aVoid -> {
+                    Toast.makeText(getContext(), "Perfil actualizado correctamente", Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(getContext(), "Error al actualizar perfil: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+
+        // Actualizar la contraseña en Firebase Authentication si ha sido modificada
+        if (!nuevaContraseña.isEmpty()) {
+            auth.getCurrentUser().updatePassword(nuevaContraseña)
+                    .addOnSuccessListener(aVoid -> Toast.makeText(getContext(), "Contraseña actualizada", Toast.LENGTH_SHORT).show())
+                    .addOnFailureListener(e -> Toast.makeText(getContext(), "Error al actualizar contraseña", Toast.LENGTH_SHORT).show());
+        }
 
     }
 
@@ -102,9 +153,10 @@ public class ProfileFragment extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (data.getData() != null){
+        // Verifica que data no sea nulo y contiene un URI válido
+        if (data != null && data.getData() != null) {
             Uri profileUri = data.getData();
-            perfilImg.setImageURI(profileUri);
+            perfilImg.setImageURI(profileUri); // Actualiza la imagen en la vista
 
             final StorageReference reference = storage.getReference().child("profile_picture")
                     .child(FirebaseAuth.getInstance().getUid());
@@ -119,12 +171,13 @@ public class ProfileFragment extends Fragment {
                         public void onSuccess(Uri uri) {
                             db.getReference().child("Usuarios").child(FirebaseAuth.getInstance().getUid())
                                     .child("profileImg").setValue(uri.toString());
-                            Toast.makeText(getContext(),"Imagen de perfil actualizada", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getContext(), "Imagen de perfil actualizada", Toast.LENGTH_SHORT).show();
                         }
                     });
                 }
             });
-        }else {
+        } else {
+            // Muestra un mensaje si no se seleccionó una imagen
             Toast.makeText(getContext(), "No se seleccionó ninguna imagen", Toast.LENGTH_SHORT).show();
         }
     }
