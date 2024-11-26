fab35bc54bc3d1e90506e86af78f7fa2ab151e00
THECRIS2 <106180525+THECRIS2@users.noreply.github.com>
2024-11-26 18:02:20 -0300

    Add files via upload
    script hecho en python para verificar el envio de datos

---
+++ mqtt.py
@@ -0,0 +1,10 @@
+import paho.mqtt.client as mqtt
+
+def on_message(client, userdata, msg):
+    print(f"Mensaje recibido: {msg.payload.decode()} en el topic {msg.topic}")
+
+client = mqtt.Client()
+client.connect("broker.emqx.io", 1883, 60) ## este es nuestro broker
+client.subscribe("lab/redes/android") ## este es nuestro topico
+client.on_message = on_message
+client.loop_forever()
