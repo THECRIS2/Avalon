ac5f1ace6a9a3279df5fba441d97abfc9a79be83
THECRIS2 <106180525+THECRIS2@users.noreply.github.com>
2024-11-27 07:58:50 -0300

    mqtt.py

--- mqtt.py
+++ mqtt.py
@@ -8,3 +8,5 @@ client.connect("broker.emqx.io", 1883, 60) ## este es nuestro broker
 client.subscribe("lab/redes/android") ## este es nuestro topico
 client.on_message = on_message
 client.loop_forever()
+
+## instalar antes la libreria de paho en la terminal con el comando pip install paho-mqtt
